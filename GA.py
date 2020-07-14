91f7107f525983a66563ea470c9d9273226dc42c
Diego Alba <d.alba.work@gmail.com>
2020-07-14 08:55:04 -0500

    removed unecessary files
    removed original chromosome file since it was too cluttered
    git commit -m

--- GA.py
+++
@@ -1,8 +0,0 @@
-from chromosome import Chromosome
-
-arr = [0.9, 81, 12, 23, 12, 15]
-
-chromo = Chromosome(arr=arr)
-print("Here is the original value: 0.9")
-print("Here is the new value: " + str(chromo.qCoffee.gene_value))
-
