25b6851663d05ad3c1cde29b001dab88765ce08d
Diego Alba <d.alba.work@gmail.com>
2020-07-15 08:40:23 -0500

    final changes to GACodeBase; moving to fileHandler
    - moving to fileHandler
    - planning for Observation
    - final changes to GA Code Base

--- GACodeBase.py
+++ GACodeBase.py
@@ -1,5 +1,6 @@
 from random import randint
 from chromosomeV2 import Chromo
+from chromosomeV2 import Gene
 
 class GA:
     def __init__(self):
@@ -15,21 +16,14 @@ class GA:
 
         crossPoint = self.selectCrossOverPoint(len(chromo1_arr))
 
-        print("CrossOver Point = %s" % (crossPoint))
-
         # splice both arrays
         preSplit1 = chromo1_arr[0:crossPoint]
-        print("preSplit1 = " + str(preSplit1)) 
         postSplit1 = chromo1_arr[crossPoint:]
-        print("postSplit1 = " + str(postSplit1)) 
         preSplit2 = chromo2_arr[0:crossPoint]
-        print("preSplit2 = " + str(preSplit2))
         postSplit2 = chromo2_arr[crossPoint:]
-        print("postSplit2 = " + str(postSplit2))
 
         # select from splices
         preSelector = randint(1,2)
-        print("preSelector = " + str(preSelector))
 
         childPre = None
         childPost = None
@@ -43,26 +37,4 @@ class GA:
             childPost = postSplit1
 
         child = Chromo(parent=(childPre + childPost))
-
-        print("\n")
-        print("childPre is = %s; childPost is = %s" % (str(childPre), str(childPost)))
-
         return child
-
-
-if __name__ == "__main__":
-    chromo1_arr = [10.2, 20.4, 30.9, 40.1]
-    chromo2_arr = [80.0, 43.2, 28.5, 11.2]
-
-    chromo1 = Chromo(chromo1_arr)
-    chromo2 = Chromo(chromo2_arr)
-
-    gA = GA()
-    child = gA.crossOver(chromo1, chromo2)
-    print("\n")
-    print("Child: "+str(child.exportToArr(child)))
-    print("\n")
-    print("Parents: " + str(chromo1.exportToArr(chromo1)))
-    print("Parents: " + str(chromo2.exportToArr(chromo2)))
-
-
